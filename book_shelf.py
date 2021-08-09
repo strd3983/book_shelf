7cc1aea047deff2555645189b3aae880dc27e556
strd3983 <strd3983@gmail.com>
2021-08-10 01:05:30 +0900

    v1.0 - 既刊情報の取得、複数シリーズの取得

--- book_shelf.py
+++ book_shelf.py
@@ -10,10 +10,11 @@ from bs4 import BeautifulSoup as bs4
 # メイン関数
 # --------------------------------------------------
 def main():
-    url = config()
-    if url == 0:
+    urls = config()
+    if urls == 0:
         return
-    book_info(url)
+    for url in urls:
+        book_info(url)
 
 
 # --------------------------------------------------
@@ -22,17 +23,18 @@ def main():
 def config():
     config_ini = configparser.ConfigParser(interpolation=None)
     config_ini_path = 'setting.ini'
+    urls = []
     # iniファイルが存在するかチェック
     if os.path.exists(config_ini_path):
         # iniファイルが存在する場合、ファイルを読み込む
         with open(config_ini_path, encoding='utf-8') as fp:
             config_ini.read_file(fp)
             # iniの値取得
-            read_default = config_ini['DEFAULT']
-            url = read_default.get('URL', raw=True)
-            url = 'https://www.amazon.co.jp/gp/product/' + url
+            for i in range(1, int(config_ini['DEFAULT']['number']) + 1):
+                url = config_ini[str(i)]['URL']
+                urls.extend(['https://www.amazon.co.jp/gp/product/' + url])
             # 設定出力
-            return url
+            return urls
     else:
         print('setting.iniが見つかりません\n')
         return 0
@@ -48,13 +50,23 @@ def book_info(url):
     }
     page = requests.get(url, headers=headers)
     soup = bs4(page.text, "html.parser")
+
+    # シリーズ情報の取得
     title = soup.find('title').get_text(strip=True).replace('Kindle版', '')
     author = soup.find('span', {'id': 'author-name'}).get_text(strip=True)
-    print('#########################################')
-    print('書籍名:', title)
+    print('\n#############################################################')
+    print('シリーズ名:', title)
     print('著者名:', author)
     print('URL:', url)
-    print('#########################################')
+    print()
+
+    # 既刊情報の取得
+    titles = soup.find_all(
+        'a',
+        {'class': 'a-size-base-plus a-link-normal itemBookTitle a-text-bold'})
+    for i, title in enumerate(titles):
+        print(i, ':', title.get_text(strip=True))
+    print('#############################################################\n')
     return
 
 
