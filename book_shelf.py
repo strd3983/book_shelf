5709bac08eaff66b2d71c7d5c10a24b73d8dc0f2
strd3983 <strd3983@gmail.com>
2021-08-10 19:21:14 +0900

    v2.0 - Seleniumに移行作業

--- book_shelf.py
+++ book_shelf.py
@@ -3,18 +3,16 @@ import requests
 import configparser
 from bs4 import BeautifulSoup as bs4
 
-# import pandas as pd
-
 
 # --------------------------------------------------
 # メイン関数
 # --------------------------------------------------
 def main():
-    urls = config()
+    urls, rnums = config()
     if urls == 0:
         return
-    for url in urls:
-        book_info(url)
+    for i in range(len(urls)):
+        book_info(urls[i], rnums[i])
 
 
 # --------------------------------------------------
@@ -24,32 +22,35 @@ def config():
     config_ini = configparser.ConfigParser(interpolation=None)
     config_ini_path = 'setting.ini'
     urls = []
+    rnums = []
     # iniファイルが存在するかチェック
     if os.path.exists(config_ini_path):
         # iniファイルが存在する場合、ファイルを読み込む
         with open(config_ini_path, encoding='utf-8') as fp:
             config_ini.read_file(fp)
             # iniの値取得
-            for i in range(1, int(config_ini['DEFAULT']['number']) + 1):
-                url = config_ini[str(i)]['URL']
+            for i in range(int(config_ini['DEFAULT']['number'])):
+                url = config_ini[str(i + 1)]['URL']
                 urls.extend(['https://www.amazon.co.jp/gp/product/' + url])
+                rnum = config_ini[str(i + 1)]['Read']
+                rnums.extend([rnum])
             # 設定出力
-            return urls
+            return urls, rnums
     else:
         print('setting.iniが見つかりません\n')
-        return 0
+        return 0, 0
 
 
 # --------------------------------------------------
 # 書籍情報の取得
 # --------------------------------------------------
-def book_info(url):
-    headers = {
+def book_info(url, rnum):
+    user_agent = {
         "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"
     }
-    page = requests.get(url, headers=headers)
-    soup = bs4(page.text, "html.parser")
+    page = requests.get(url, headers=user_agent)
+    soup = bs4(page.text, "html5lib")
 
     # シリーズ情報の取得
     title = soup.find('title').get_text(strip=True).replace('Kindle版', '')
@@ -64,8 +65,10 @@ def book_info(url):
     titles = soup.find_all(
         'a',
         {'class': 'a-size-base-plus a-link-normal itemBookTitle a-text-bold'})
-    for i, title in enumerate(titles):
-        print(i, ':', title.get_text(strip=True))
+    for i in range(len(titles)):
+        if i == int(rnum):
+            print('------------------------------------------------------')
+        print(i + 1, ':', titles[i].get_text(strip=True))
     print('#############################################################\n')
     return
 
---
+++ building.ps1
@@ -0,0 +1,1 @@
+pyinstaller .\book_shelf.py --onefile --noconsole --add-binary ".\bin\chromedriver.exe;.\driver" --add-binary ".\browser;.\browser"
